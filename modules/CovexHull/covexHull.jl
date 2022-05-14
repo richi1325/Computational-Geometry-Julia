332c874304d07c988b84696f701aed2abe83c0cc
angel1990 <angelyairchavez@gmail.com>
2022-05-14 15:51:15 -0500

    QuickHull

--- modules/CovexHull/covexHull.jl
+++ modules/CovexHull/covexHull.jl
@@ -287,10 +287,95 @@ function convexHullGiftWrapping( points )
         p = q
             
     end
- 
     
     return hull;
         
+end
+
+function findSide(p1, p2, p)
+
+    val = (p.y - p1.y) * (p2.x - p1.x) - (p2.y - p1.y) * (p.x - p1.x);
+  
+    if (val > 0)
+        return 1
+    end
+    if (val < 0)
+        return -1
+    end
+    return 0
+    
+end
+  
+function lineDist( p1, p2, p)
+
+    return abs((p.y - p1.y) * (p2.x - p1.x) - (p2.y - p1.y) * (p.x - p1.x))
+    
+end
+  
+function quickHull(points, n, p1, p2, side, hull)
+    ind = 0
+    max_dist = 0
+  
+    for i in 1:n
+        temp = lineDist(p1, p2, points[i]);
+        if (findSide(p1, p2, points[i]) == side) && (temp > max_dist)
+            ind = i;
+            max_dist = temp;
+        end
+    end
+  
+    if ind == 0 
+        
+        if !(p1 in hull)
+            push!(hull,p1)
+        end
+        
+        if !(p2 in hull)
+            push!(hull,p2)
+        end
+        
+        return nothing
+        
+    end
+        
+    quickHull(points, n, points[ind], p1, -findSide(points[ind], p1, p2), hull)
+    quickHull(points, n, points[ind], p2, -findSide(points[ind], p2, p1), hull)
+        
+end
+  
+function convexHullQuickHull(points)
+
+    n = length(points)
+
+    if (n < 3)
+        return []
+    end  
+    
+    min_x = 1
+    max_x = 1
+            
+    for i in 2:n
+                
+        if (points[i].x < points[min_x].x)
+            min_x = i
+        end
+                
+        if (points[i].x > points[max_x].x)
+            max_x = i
+        end
+                
+    end
+  
+    hull = []
+            
+    quickHull(points, n, points[min_x], points[max_x], 1, hull);
+  
+    quickHull(points, n, points[min_x], points[max_x], -1, hull);
+  
+    quickSort(hull, 2, length(hull) , hull[ 1 ])
+    
+    return hull
+                
 end
 
 
