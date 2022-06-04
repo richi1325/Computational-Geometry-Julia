53a091bb9eedd3be0e5b13da4697a49f87a34e85
Jose Ricardo Mendoza Villar <richi_1325@hotmail.com>
2022-06-04 01:38:04 -0500

    feature: restructuración y agregar paquetes faltantes

--- packages.jl
+++ packages.jl
@@ -2,7 +2,9 @@ using Pkg
 
 dependencies = [
     Pkg.PackageSpec(;name="IJulia", version="1.23.3"),
-    Pkg.PackageSpec(;name="Plots", version="1.25.7")
+    Pkg.PackageSpec(;name="Plots", version="1.25.7"),
+    Pkg.PackageSpec(;name="DataStructures", version="0.18.13"),
+    Pkg.PackageSpec(;name="LazySets", version="1.57.0")    
 ]
 
 Pkg.add(dependencies)
